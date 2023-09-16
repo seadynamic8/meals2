5723dd39e21dcdf339372b4487f6860a00d7943e
seadynamic8 <seadynamic8@zoho.com>
2023-09-16 14:37:19 -0400

    Add some styling on meal item and added meal item traits

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals2/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import 'package:meals2/models/meals.dart';
@@ -7,6 +8,16 @@ class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +32,51 @@ class MealItem extends StatelessWidget {
             left: 0,
             right: 0,
             bottom: 0,
-            child: Column(
-              children: [
-                Text(meal.title),
-              ],
+            child: Container(
+              color: Colors.black54,
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+              child: Column(
+                children: [
+                  Text(
+                    meal.title,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MealItemTrait(
+                        icon: Icons.schedule,
+                        label: '${meal.duration} min',
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      MealItemTrait(
+                        icon: Icons.work,
+                        label: complexityText,
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      MealItemTrait(
+                        icon: Icons.attach_money,
+                        label: affordabilityText,
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ],
